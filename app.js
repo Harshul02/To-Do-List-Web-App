cbe117604dc2abf4d7713aa6a1002b42341a5bb5
Harshul Jain <harshulgangwal02@gmail.com>
2022-08-17 23:59:27 +0530

    Added ejs files and created work route

--- app.js
+++ app.js
@@ -5,35 +5,57 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-var items =["Buy Food", "Cook Food", "Eat Food"];
+let items =["Buy Food", "Cook Food", "Eat Food"];
+
+let workItems = [];
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static("public"));
+
 app.get("/",function(req,res)
 {
-    var today = new Date();
+    let today = new Date();
     
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day=today.toLocaleDateString("en-US", options);
+    let day=today.toLocaleDateString("en-US", options);
 
     
-
-   res.render("list", {kindOfDay: day, newListItems: items});
+   res.render("list", {listTitle: day, newListItems: items});
 })
 
+app.get("/work", function(req,res)
+{
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
+app.post("/work", function(req,res)
+{
+    let item=req.body.newItem;
+
+    workItems.push(item);
+    res.redirect("/work");
+})
 
 app.post("/", function(req,res)
 {
-    var item = req.body.newItem;
+    let item = req.body.newItem;
+    // console.log(req.body.list);
+    if(req.body.list === "Work")
+    {
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
 
-    items.push(item);
-    
-    res.redirect("/");
 })
 
 
