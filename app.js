17a0d904270314659fe9d23577b53bdd425525a1
Harshul Jain <harshulgangwal02@gmail.com>
2022-12-04 16:58:41 +0530

    Vercel

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 
